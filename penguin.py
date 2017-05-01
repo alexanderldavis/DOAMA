232cf825eb9a7989255388087b92fe9daf51bc81
Alexander Davis <alexander.davis54@gmail.com>
2017-05-01 01:36:18 -0500

    penguin.py now populates tables on knuth, we can change this to a heroku project this tuesday

--- penguin.py
+++ penguin.py
@@ -1,18 +1,25 @@
+import psycopg2
+from flask import Flask, render_template, request
+import os
 from bs4 import BeautifulSoup
 import requests as req
 import json
+os.system("ssh -f -L 2345:localhost:5432 -N knuth.luther.edu")
 
-# t=requests.get('http://www.theyshootpictures.com/gf1000_a-b.htm')
+app = Flask(__name__)
+conn = psycopg2.connect(dbname='davial02', user='davial02', host='localhost', port='2345')
+
+#################### CREATE TABLES / KNUTH ####################
+cur = conn.cursor()
+cur.execute("DROP table movies CASCADE; DROP table genres CASCADE; DROP table genres_movies CASCADE;")
+cur.execute("CREATE table movies (id serial unique, title varchar(100), description text); CREATE table genres (id serial unique, name varchar(20)); CREATE table genres_movies (movieid int, genreid int, FOREIGN KEY (movieid) references movies(id), FOREIGN KEY (genreid) references genres(id), primary key (movieid, genreid));")
+conn.commit()
 t = req.get('http://www.theyshootpictures.com/gf1000_all1000films_table.php')
 soup=BeautifulSoup(t.text)
 soup.prettify()
-file = open("text.casm", "w")
-file.write(t.text)
 data=soup.find_all("td", { "class" : "csv_column_3" })
-dataDict={}
-columns=data[0].get_text().split('\n')
-for i in range(1,len(columns)-1):
-    dataDict[columns[i]]=[]
+totalnumoffilms=0
+genreList = []
 for i in range(1,len(data)):
     movieName = data[i].get_text()
     finalString = ""
@@ -24,22 +31,28 @@ for i in range(1,len(data)):
         movieName = finalString[:-1]
     if "' " in movieName:
         movieName = movieName.replace("' ", "'")
-    print(movieName)
     movieName = movieName.replace(" ", "+")
     res = req.get("http://www.omdbapi.com/?t={}".format(movieName))
-    
-    print(res.text)
+    dataParsed = json.loads(res.text)
+    if dataParsed["Response"] != "False":
+        totalnumoffilms+=1
+        cur.execute("""INSERT INTO movies (title, description) VALUES (%s, %s);""", (dataParsed["Title"],dataParsed["Plot"]))
+
+        print(dataParsed["Title"])
+        genres = dataParsed["Genre"]
+        genres = genres.split(", ")
+        for genre in genres:
+            if genre not in genreList:
+                cur.execute("""INSERT INTO genres (name) VALUES (%s)""", (genre,))
+                genreList.append(genre)
+            # TODO: ASK ABOUT USING RETURNING IN SQL TO GET THE MOVIEID
+            cur.execute("""INSERT INTO genres_movies (movieid, genreid) VALUES (%s,(SELECT id FROM genres WHERE name = %s))""", (str(totalnumoffilms), genre))
+    conn.commit()
+print("===============================INFO===============================")
+print("All Genres:", genreList)
+print("Total Num of Films:", totalnumoffilms-1)
 
 
-
-    # x=data[i].find_all("td")
-    # dataDict['Rank'].append(x[0].get_text())
-    # dataDict['College'].append(x[1].get_text())
-    # dataDict['Freshman class'].append(x[2].get_text())
-    # dataDict['Pell grad share'].append(x[3].get_text())
-    # dataDict['Net price, middle- income'].append(x[4].get_text())
-    # dataDict['College Access Index'].append(x[5].get_text())
-    # dataDict['Endowment per student'].append(x[6].get_text())
-# collegeData=pd.DataFrame(dataDict)
-# collegeData=collegeData[['Rank','College','Freshman class','Pell grad share','Net price, middle- income','College Access Index','Endowment per student']]
-# print(collegeData)
+#NOTE: SOME FYI INFO:
+# ALL GENRES: ['Mystery', 'Romance', 'Thriller', 'Adventure', 'Sci-Fi', 'Comedy', 'Drama', 'Short', 'Crime', 'Western', 'War', 'History', 'Biography', 'Documentary', 'Music', 'Sport', 'Horror', 'Film-Noir', 'Fantasy', 'Action', 'Family', 'Animation', 'Musical', 'N/A']
+# 932 MOVIES COMPILE CORRECTLY / ASSOCIATE WITH THE API
