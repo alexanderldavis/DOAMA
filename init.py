7df988ce9740c36df44ade27019e5a7bf4c8225c
Alexander Davis <alexander.davis54@gmail.com>
2017-05-06 14:18:17 -0500

    add requirements canistreamit

--- init.py
+++ init.py
@@ -54,9 +54,9 @@ for i in range(1,len(data)):
     if dataParsed["Response"] != "False":
         # search for availability in different gate
         movie=search(dataParsed['Title'])[0]
-        streamingList=movie['_id'].keys()
-        rentalList=movie['_id'].keys()
-        purchaseList=movie['_id'].keys()
+        streamingList=stream(movie['_id']).keys()
+        rentalList=rental(movie['_id']).keys()
+        purchaseList=purchase(movie['_id']).keys()
         dvdList=dvd(movie['_id'])
         cableList=xfinity(movie['_id'])
         for li in [streamingList,rentalList,purchaseList,dvdList,cableList]:
