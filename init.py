c30ad22b36b0118048740ae44600d7611c671d2d
Linh Nguyen Le <nguyli03@.luther.edu>
2017-05-07 11:06:05 -0500

    activties_movies

--- init.py
+++ init.py
@@ -15,20 +15,29 @@ db = psycopg2.connect(database=url.path[1:],user=url.username,password=url.passw
 conn = db
 #################### CREATE TABLES / KNUTH ####################
 cur = conn.cursor()
-cur.execute("DROP table services CASCADE; DROP table services_movies CASCADE; DROP table movies CASCADE; DROP table genres CASCADE; DROP table genres_movies CASCADE; DROP table actors CASCADE; DROP table actors_movies CASCADE;")
+cur.execute("DROP table services CASCADE; DROP table acitvities CASCADE; DROP table activities_movies CASCADE;DROP table services_movies CASCADE; DROP table movies CASCADE; DROP table genres CASCADE; DROP table genres_movies CASCADE; DROP table actors CASCADE; DROP table actors_movies CASCADE;")
 print("TABLES DELETED")
 cur.execute("""CREATE table movies (id serial unique, title varchar(100), description text, year varchar(40), rated varchar(50), runtime varchar(50), poster varchar(200));""")
 cur.execute("""CREATE table genres (id serial unique, name varchar(20)); CREATE table genres_movies (movieid int, genreid int, FOREIGN KEY (movieid) references movies(id), FOREIGN KEY (genreid) references genres(id), primary key (movieid, genreid));""")
 cur.execute("""CREATE table actors (id serial unique, name varchar(99)); CREATE table actors_movies (movieid int, actorid int, FOREIGN KEY (movieid) references movies(id), FOREIGN KEY (actorid) references actors(id), primary key (movieid, actorid));""")
 cur.execute("""CREATE table services (id serial unique, name text);""")
+cur.execute("""CREATE table activities (id serial unique, name text);""")
 cur.execute("""CREATE table services_movies (movie_id int, FOREIGN key(movie_id) references movies(id), service_id int, FOREIGN key(service_id) references services(id));""")
+cur.execute("""CREATE table activities_movies (movie_id int, FOREIGN key (movie_id) references movies(id), activity_id text, FOREIGN key(activity_id) references activities(id));""")
 print("TABLES CREATED")
 conn.commit()
+# create table for streaming channels
 watchingOptions=['netflix_instant','amazon_prime_instant_video','hulu_movies','crackle','youtube_free','epix','streampix','snagfilms','fandor_streaming','amazon_video_rental','apple_itunes_rental','android_rental','vudu_rental','youtube_rental','sony_rental','vimeo_vod_rental','amazon_video_purchase','apple_itunes_purchase','android_purchase','vudu_purchase','xbox_purchase','sony_purchase','vimeo_vod_purchase\
 ','amazon_dvd','amazon_bluray','netflix_dvd','redbox','hbo','showtime','cinemax','starz','encore','xfinity_free']
 for option in watchingOptions:
     cur.execute("""INSERT INTO services(name) VALUES (%s);""",(option,))
     print(option)
+
+# create tables for activity:
+activityList=['Family night','Girls night','Date night','Nerd night',"Guys' party",'Cultured movie night','Surprise me']
+for activity in activityList:
+    cur.execute("""INSERT into acitivies(name) VALUES (%s);"""%activity)
+
 t = req.get('http://www.theyshootpictures.com/gf1000_all1000films_table.php')
 print("LIST SCRAPED FROM SOURCE")
 soup=BeautifulSoup(t.text)
@@ -53,6 +62,12 @@ for i in range(1,len(data)):
     dataParsed = json.loads(res.text)
 
     if dataParsed["Response"] != "False":
+        totalnumoffilms+=1
+        if dataParsed["Poster"] != "N/A":
+            cur.execute("""INSERT INTO movies (title, description, year, rated, runtime, poster) VALUES (%s, %s, %s, %s, %s, %s);""", (dataParsed["Title"],dataParsed["Plot"],dataParsed["Year"],dataParsed["Rated"], dataParsed["Runtime"],dataParsed["Poster"]))
+        else:
+            cur.execute("""INSERT INTO movies (title, description, year, rated, runtime, poster) VALUES (%s, %s, %s, %s, %s, %s);""", (dataParsed["Title"],dataParsed["Plot"],dataParsed["Year"],dataParsed["Rated"], dataParsed["Runtime"],"http://www.projectdoama.com/static/penguin.jpg"))
+        print("Added: ",dataParsed["Title"])
         print(dataParsed['Title'])
         # search for availability in different gate
         movie=search(dataParsed['Title'])[0]
@@ -68,12 +83,6 @@ for i in range(1,len(data)):
                 for key in keys:
                     # cur.execute("""INSERT INTO services(name) VALUES (%s);""",(li[key]))
                     cur.execute("""INSERT INTO services_movies(movie_id,service_id) VALUES ((select id from movies where title=%s),(select id from services where name=%s));""",(dataParsed['Title'],key))
-        totalnumoffilms+=1
-        if dataParsed["Poster"] != "N/A":
-            cur.execute("""INSERT INTO movies (title, description, year, rated, runtime, poster) VALUES (%s, %s, %s, %s, %s, %s);""", (dataParsed["Title"],dataParsed["Plot"],dataParsed["Year"],dataParsed["Rated"], dataParsed["Runtime"],dataParsed["Poster"]))
-        else:
-            cur.execute("""INSERT INTO movies (title, description, year, rated, runtime, poster) VALUES (%s, %s, %s, %s, %s, %s);""", (dataParsed["Title"],dataParsed["Plot"],dataParsed["Year"],dataParsed["Rated"], dataParsed["Runtime"],"http://www.projectdoama.com/static/penguin.jpg"))
-        print("Added: ",dataParsed["Title"])
         genres = dataParsed["Genre"]
         genres = genres.split(", ")
         for genre in genres:
@@ -82,6 +91,21 @@ for i in range(1,len(data)):
                 genreList.append(genre)
             # TODO: ASK ABOUT USING RETURNING IN SQL TO GET THE MOVIEID
             cur.execute("""INSERT INTO genres_movies (movieid, genreid) VALUES (%s, (SELECT id FROM genres WHERE name = %s))""", (str(totalnumoffilms), genre))
+        # create entries for activities_movies table
+        if "Adventure" in genres or "Comedy" in genres or "Mistery" in genres or "Fantasy" in genres or "Animation" in genres:
+            if dataParsed['Rated']=="PG13" or dataParsed['Rated']=='PG':
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Family night"))"""%dataParsed['Title'])
+        elif "Romance" in genres or "Drama" in genres or "Comedy" in genres oor "Fantasy" in genres or "Animation" in genres:
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Girls night"))"""%dataParsed['Title'])
+        elif "Romance" in genres or "Horror" in genres or "Drama" in genres or "Thriller" in genres or "Fantasy" in genres or "Animation" in genres:
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Date night"))"""%dataParsed['Title'])
+        elif "Mistery" in genres or "Sci-Fi" in genres or "Crime" in genres or "Comedy" in genres or "Adventure"in genres or "Documentary" in genres or "War" in genres or "Biography" in genres:
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Nerd night"))"""%dataParsed['Title'])
+        elif "Comedy" in genres or "Horror" in genres or "Thriller" in genres or "Adventure" in genres:
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Guys' party"))"""%dataParsed['Title'])
+        elif "Western" in genres or "War" in genres or "Short" in genres:
+                cur.execute("""INSERT INTO activities_movies(movie_id,activity_id) VALUES ((SELECT id FROM movies WHERE title=%s),(SELECT id FROM activity WHERE name="Cultured movie night"))"""%dataParsed['Title'])
+
         actors = dataParsed["Actors"]
         actors = actors.split(", ")
         for actor in actors:
