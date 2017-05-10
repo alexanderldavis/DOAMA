e3247fad80a72af3f053507e16ff5471bc3bf300
Linh Nguyen Le <nguyli03@.luther.edu>
2017-05-09 23:51:01 -0500

    route to the right page

--- router.py
+++ router.py
@@ -105,8 +105,6 @@ def searchMovie():
     res=db.session.execute("""SELECT movie.id, movie.title, movie.description, movie.poster, movie.rated, movie.rating from movie where movie.title like'%%%s%%' limit 1;"""%movie)
     res=res.fetchall()
     return render_template('getmovieinfo.html',movie=res,activity=movie)
-
-@app.route('/searchGenre')
 # import psycopg2
 # from flask import Flask, render_template, request
 # import os
