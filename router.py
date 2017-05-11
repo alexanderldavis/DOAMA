0a5232a03072ba7f5c65092284d6254f640882db
Alexander Davis <alexander.davis54@gmail.com>
2017-05-10 21:20:39 -0500

    tets

--- router.py
+++ router.py
@@ -158,7 +158,7 @@ def goodFor():
     res=res.fetchall()
     return render_template('searchresults.html',movieList=res,activity="")
 
-@app.route("/addMovie", methods=["POST"])
+@app.route("/addMovie", methods=["GET"])
 def addMovieToDb():
     movieName=request.args['movieTitleAdd']
     res=db.session.execute("""SELECT count(*) from movie where title='%s'"""%movieName)
